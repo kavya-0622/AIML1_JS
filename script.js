8c990772e92921bff9a449eed3592b50777d3253
Kavya Sharma <kavya22666@gmail.com>
2026-02-28 23:17:11 +0530

    JavaScript File

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-console.log("Hello World")
->>document.write("Hello World");
-var num = 10;
-console.log(num);
-console.log(typeof(num));
-num = false;
-console.log(num);
-console.log(typeof(num));
-num = "Hello";
-console.log(num);
-console.log(typeof(num));
-num = null;
-console.log(num);
-console.log(typeof(num));
-num = undefined;
-console.log(num);
-console.log(typeof(num));
+// console.log("Hello World")
+// >>document.write("Hello World");
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+// num = "Hello";
+// console.log(num);
+// console.log(typeof(num));
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
+// num = undefined;
+// console.log(num);
+// console.log(typeof(num));
+
+var count;
+document.write("Starting Loop"+"<br/>");
+for(count = 0;count<10;count++){
+    document.write("Starting Loop"+"<br/>");
+    document.write("<br/>");
+}
+document.write("Loop Stopped");
