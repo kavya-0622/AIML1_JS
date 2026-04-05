03fdf9d6d85eef5484277940547d318bb978123d
Kavya Sharma <kavya22666@gmail.com>
2026-04-05 22:02:02 +0530

    Exp 1

---
+++ script3.js
@@ -0,0 +1,41 @@
+const element = document.getElementById("myH1");
+let text = element.getAttribute("democlass");
+document.getElementById("demo").innerHTML = text;
+
+
+function myFunction(){
+    document.getElementById("myH1").setAttribute("class", "democlass")
+}
+
+// Create Element
+// const heading = document.createElement("h2");
+// //get the parent
+// const bodytag = document.getElementByTagName("body");
+// //add element at the position using append method
+// bodytag[0].append(heading);
+// heading.innerText="Hello";headingstyle.background-color="red"
+
+
+// function practice(){
+//     document.open();
+//     document.write("<h1>")
+// }
+
+
+
+// function onMouseOver(){
+//     console.log("ON Mouse Over")
+// ;}
+
+
+// const x = document.querySelector("input");
+// x.addEventListener("focus", onhandlefocus);
+// x.addEventListener("blur", onhandleblur);
+
+// function onhandlefocus(){
+//     x.style.background="Red";
+// }
+// function onhandlefocus(){
+//     x.style.background="Yellow";
+//     x.style.background="Green";
+// }
